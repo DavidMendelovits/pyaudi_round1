0938c65c549a4b19b384ba3f80f3c1ccc9085287
David Mendelovits <david.mendelovits@gmail.com>
2018-12-11 15:31:52 -0800

    I have no idea what I am doing

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # pyaudi_round1
 First foray into pyaudio for Otosense's impossible challenge
+-To run: 'python impossible_test.py' (records 5 seconds of audio from computer mic, and outputs .wav file)
+->file prints out the sum of every two bytes of information
---
+++ impossible_test.py
@@ -0,0 +1,42 @@
+import pyaudio
+import wave
+
+CHUNK = 2048
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+RECORD_SECONDS = 5
+WAVE_OUTPUT_FILENAME = "output.wav"
+
+p = pyaudio.PyAudio()
+
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK)
+
+print("* recording")
+
+frames = []
+
+for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    data = stream.read(CHUNK)
+    for i in range(0, len(data)):
+        if (i > 0):
+            print(ord(data[i]) + ord(data[i - 1]))
+    frames.append(data)
+
+print("* done recording")
+
+stream.stop_stream()
+stream.close()
+p.terminate()
+
+wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+wf.setnchannels(CHANNELS)
+wf.setsampwidth(p.get_sample_size(FORMAT))
+wf.setframerate(RATE)
+wf.writeframes(b''.join(frames))
+wf.close()
+
